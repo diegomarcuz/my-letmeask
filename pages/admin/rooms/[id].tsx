b3018e3a141adf0d0b9b2f1b87efb6baa0b8c42d
diegomarcuz <diego.marcuz@hotmail.com>
2021-07-11 12:39:10 -0300

    fix(Admin): responsive, translate, user actions

--- pages/admin/rooms/[id].tsx
+++ pages/admin/rooms/[id].tsx
@@ -1,196 +1,165 @@
-import {  useRouter } from "next/router"
+import { useRouter } from "next/router"
 import Image from 'next/image'
-import { styled } from '@stitches/react';
+import { styled } from '../../../stitches.config';
+import toast, { Toaster } from 'react-hot-toast'
 
 import logoImg from "../../../public/images/logo.svg"
 import deleteImg from "../../../public/images/delete.svg"
 
 import { Button } from "../../../components/Button"
 import { Question } from "../../../components/Question"
-
+import { useAuth } from "../../../hooks/useAuth"
 import { useRoom } from "../../../hooks/useRoom"
-import {RoomCode} from '../../../components/RoomCode'
+import { RoomCode } from '../../../components/RoomCode'
 
 import { firebase } from "../../../service/firebase"
 
 const Header = styled("header", {
-  borderBottom: '1px solid #E2E2E2',
+  borderBottom: '1px solid $grayDark',
+  background: "$background",
   display: "flex",
   justifyContent: 'space-around',
   alignItems: 'center',
-  flexWrap: 'wrap',
-  padding: '0 32px',
-  minHeight: '120px',
-  marginBottom: '32px',
-  "div.actions":{
-    display: "flex",
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    flexWrap: 'wrap',
-    width: '440px',
-    margin: '0 16px',
-    "button":{
-      '@media (max-width: 760px)':{
-        margin: '8px 0',
-      }
-    }
-    
+  flexFlow: 'row wrap',
+  padding: '1rem 2rem',
+  minHeight: '7.5rem',
+ 
 
-  },
+})
 
-  'button': {
-    height: '40px',
-    borderRadius: '8px',
-    overflow: 'hidden',
-  
-    background: '#fff',
-    border:' 1px solid #835afd',
-    cursor: 'pointer',
-  
-    display: 'flex',
-  
-    'div': {
-      background: '#835afd',
-      padding: '5px 6px 2px 6px',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100%',
-    },
-  
-    'span': {
-      display: 'block',
-      alignSelf: 'center',
-      flex: 1,
-      padding: '0 16px 0 12px',
-      width: '230px',
-      fontSize: '14px',
-      fontWeight: 500,
-    }
-
-
+const HeaderActions = styled("section", {
+  display: "flex",
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  flexFlow: 'row wrap',
+  flex: 1,
+  width: "100%",
+  marginLeft: '24px',
+  '@tablet':{
+   
+    justifyContent: 'space-around',
   }
-
-
-
-
 })
 
 
 const Main = styled('main', {
-  margin: '0 16px',
-  "@media (min-width: 720px)":{
-    maxWidth: '720px',
-    width: '100%',
-    margin: '0 auto',
-    
-  }, 
+  background: "$secondBackground",
+  height: 'calc(100vh - 110px)',
+  paddingTop: '2rem',
 
+})
+const RoomInfoSection = styled("section", {
+
+  margin: '0 32px',
+  "@tablet": {
+    maxWidth: '45rem',
+    width: '100%',
+    margin: '2rem auto',
+
+  },
   "h2": {
-    fontSize: "20px",
+    fontSize: "2rem",
     fontWeight: 900,
   },
-  "form": {
-    margin: '24px 0',
-
-    "textarea": {
-      maxWidth: '100%',
-      width: '100%',
-      resize: 'vertical',
-      height: '100px',
-      borderRadius: '5px',
-      border: 0,
-      padding: '8px',
-      boxShadow: '0 2px 8px 0 rgba(0,0,0,0.4)',
-
-      "&:focus": {
-        borderColor: '#835AFD'
-      }
-
-    },
-    "section.footer": {
-      marginTop: '24px',
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-
-      "p": {
-        fontSize: '14px',
-        color: '#737380',
-        "button": {
-          marginLeft: '3px',
-          background: 'transparent',
-          border: 0,
-          color: '#835AFD',
-          textDecoration: 'underline'
-        }
-
-      },
-
-    }
-
+  "span": {
+    color: "$grayLight"
   }
+})
+const QuestionListSection = styled("section", {
+  margin: '0 32px',
+  "@tablet": {
+    maxWidth: '45rem',
+    width: '100%',
+    margin: '2rem auto',
 
+  },
+  "h2": {
+    fontSize: "1.5rem",
+    fontWeight: 900,
+    margin: "16px 0"
+  }
 
 })
 
 export default function AdminRoom() {
+  const { user, signIn } = useAuth()
   const router = useRouter()
   const roomId = router.query.id as string
 
-  const { title, questions } = useRoom(roomId)
+  const { title, questions, authorId } = useRoom(roomId)
 
   async function handleEndRoom() {
+   const finishToast = toast.loading("Finishing...")
+
     await firebase.database().ref(`rooms/${roomId}`).update({
       endedAt: new Date()
     })
 
+    toast.success("Room Finished!!", {id: finishToast})
     router.push('/')
   }
 
   async function handleDeleteQuestion(questionid: string) {
-    if (window.confirm('Tem certeza que você deseja excluir esta pergunta?')) {
+   
+    if (window.confirm('Do you really want to remove the question ?')) {
       await firebase.database().ref(`rooms/${roomId}/questions/${questionid}`).remove()
     }
   }
 
   return (
     <>
+    
       <Header>
-          <Image src={logoImg} alt="Letmeask" />
-          <div className="actions">
-            <RoomCode code={roomId}/>
-            <Button 
+        <a href="/">
+          <Image src={logoImg} alt="" />
+        </a>
+        <HeaderActions>
+          <RoomCode code={roomId} />
+          { (user?.id !== authorId) && !user  ? (
+              <Button  isOutlined type="button" onClick={signIn}>Sign in</Button>
+            ) :(
+              <Button
               isOutlined
               onClick={() => handleEndRoom()}
-            >
-              Encerrar sala
-            </Button>
-          </div>
+              >
+                Finish Room
+              </Button>
+            )
+          }
+      
+        </HeaderActions>
       </Header>
 
       <Main>
-        <div className="room-title">
-          <h1>Sala {title}</h1>
-          { questions.length > 0 && <span>{questions.length} pergunta(s)</span> }
-        </div>
+        <RoomInfoSection>
+          <h2>Room: {title}</h2>
+          {questions.length > 0 && <span>{questions.length} room(s)</span>}
+        </RoomInfoSection>
 
-        <div className="question-list">
+        <QuestionListSection>
+          <h2>Questions</h2>
+          
           {questions.map(question => {
             return (
-              <Question 
+              <Question
                 key={question.id}
                 content={question.content}
                 author={question.author}
               >
-                <button type="button" onClick={() => handleDeleteQuestion(question.id)}>
-                  <Image src={deleteImg} alt="Remover pergunta" />
-                </button>
+                {
+                   user && (user?.id === authorId) && (
+                    <button type="button" onClick={() => handleDeleteQuestion(question.id)}>
+                    <Image src={deleteImg} alt="Remove question" />
+                   </button>
+                  ) 
+                }
+               
               </Question>
             )
           })}
-        </div>
+        </QuestionListSection>
       </Main>
-      </>
+      <Toaster />
+    </>
   )
 }
